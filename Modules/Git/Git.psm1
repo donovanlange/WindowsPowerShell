134c51700bf2d890424b298cb11a814483614ac4
Donovan Lange <dolange@microsoft.com>
2016-02-14 08:40:57 -0800

    Let GitHub do it's own thing instead of being clever.  Stop guessing special paths

--- Modules/Git/Git.psm1
+++ Modules/Git/Git.psm1
@@ -1,9 +1,6 @@
 function Initialize-Git {
 [CmdletBinding()]
-Param(
-	[switch]
-	$SkipSSHSetup = $false
-)
+Param()
 <#
 .SYNOPSIS
 	Sets up the GitHub Git Shell Environment
@@ -12,44 +9,7 @@ Param(
 #>
 	if ($env:github_shell -eq $null) 
 	{
-		Write-Verbose "Running GitHub\shell.ps1"
-
-		if (Test-Path -Type Container "$env:LocalAppData\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad")
-		{
-			$env:github_git = Resolve-Path "$env:LocalAppData\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad"
-		}
-		else
-		{
-			return
-		}
-
-		# Initialize Environment variables needed by MinGW, SSH, and Git commands:
-		$env:PLINK_PROTOCOL = "ssh"
-		$env:TERM = "msys"
-		$env:HOME = $HOME
-		$env:OPENSSL_CONF = "$env:GITHUB_GIT\ssl\openssl.cnf"
-
-		# Setup PATH with the Git, GitHub, and msBuild tools
-		$pGitPath = $env:github_git
-		$env:Path = "$env:Path;$pGitPath\cmd;$pGitPath\bin;$pGitPath\mingw\bin"
-
-		if (Test-Path -Type Container "$env:LocalAppData\Apps\2.0\CPQE4QMB.8J3\DNL3QV1K.P41\gith..tion_317444273a93ac29_0002.000e_114545a4195e25d4")
-		{
-			$appPath = Resolve-Path "$env:LocalAppData\Apps\2.0\CPQE4QMB.8J3\DNL3QV1K.P41\gith..tion_317444273a93ac29_0002.000e_114545a4195e25d4"
-			$env:Path = "$env:Path;$appPath"
-		}
-
-		if (Test-Path -Type Container "$env:SystemRoot\Microsoft.NET\Framework\v4.0.30319")
-		{
-			$msBuildPath = "$env:SystemRoot\Microsoft.NET\Framework\v4.0.30319"
-			$env:Path = "$env:Path;$msBuildPath"
-		}
-
-		# Setup SSH:
-		if (Get-Command "GitHub.exe" -ErrorAction SilentlyContinue)
-		{
-			& GitHub.exe --set-up-ssh
-		}
+        	. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
 
 		Import-Module Posh-Git
 		Enable-GitColors
