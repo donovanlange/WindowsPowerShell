f41f281d1f48b7c24efd28ce884383cdced22feb
Donovan Lange <dolange@microsoft.com>
2016-02-16 06:23:54 -0800

    Ensure TEMP and TMP don't end with a trailing slash

--- Modules/Git/Git.psm1
+++ Modules/Git/Git.psm1
@@ -9,7 +9,10 @@ Param()
 #>
 	if ($env:github_shell -eq $null) 
 	{
-        	. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
+		. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
+
+		# Shell.ps1 overwrites TMP and TEMP with a version with a trailing '\'
+ 		$env:TMP = $env:TEMP = [system.io.path]::gettemppath().TrimEnd('\')
 
 		Import-Module Posh-Git
 		Enable-GitColors
