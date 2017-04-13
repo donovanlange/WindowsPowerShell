08a70cef22119221046a66181f2dcdde1db1ed62
Donovan Lange <dolange@microsoft.com>
2017-04-13 14:28:34 -0700

    Ensure Github's Git.exe is always first in the path

--- Modules/Git/Git.psm1
+++ Modules/Git/Git.psm1
@@ -11,13 +11,20 @@ Param()
 	{
 		. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
 
+		# Ensure that the portable git exe is first in the path:
+		if ($env:github_git)
+		{
+			$pGitPath = $env:github_git
+			$env:Path = "$pGitPath\cmd;$env:Path"
+		}
+
 		# Shell.ps1 overwrites TMP and TEMP with a version with a trailing '\'
  		$env:TMP = $env:TEMP = [system.io.path]::gettemppath().TrimEnd('\')
 
 		Import-Module Posh-Git
 		Enable-GitColors
 		Start-SshAgent -Quiet
-		
+
 		# We prefer hub.exe to git.exe, if available
 		if (Get-Command "hub.exe" -ErrorAction SilentlyContinue)
 		{
